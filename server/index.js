c35b05c8bc482c6a2135614fd3fffb45d7215569
Ravi Kiran <krpkrpkrpkrp942@gmail.com>
2025-07-29 00:07:18 +0530

    Update MongoDB connection log message

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
 // Placeholder for routes
